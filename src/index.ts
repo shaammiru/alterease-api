9c7842151073b7d219446f122e06d00dd593d073
shaammiru <msyamil.hamami@gmail.com>
2024-04-28 22:57:25 +0700

    Setup sharp

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
+import sharp from "sharp";
+// import ffmpeg from "fluent-ffmpeg";
 
 const app = new Hono();
+
+app.use("/uploads/*", serveStatic({ root: "./" }));
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
@@ -9,15 +14,34 @@ app.get("/", (c) => {
 app.post("/image/upload", async (c) => {
   const body = await c.req.parseBody();
   const file = body["image"] as Blob;
+  const width = parseInt(body["width"] as string);
+  const height = parseInt(body["height"] as string);
 
   if (file.type.split("/")[0] != "image") {
-    return c.text("File not allowed");
+    return c.text("file must be an image");
   }
 
-  return c.json({
-    message: "Image uploaded",
-    image: { name: file.name, size: file.size },
-  });
+  if (isNaN(height) || isNaN(width)) {
+    return c.text("width or height must be a decimal number");
+  }
+
+  try {
+    const buffer = await file.arrayBuffer();
+    const image = sharp(new Uint8Array(buffer));
+    const processedImage = await image
+      .resize(width, height)
+      .toFile(`uploads/image/${file.name}`);
+
+    return c.json({
+      message: "Image uploaded",
+      image: {
+        name: file.name,
+        size: file.size,
+      },
+    });
+  } catch (error) {
+    return c.text("Error processing image");
+  }
 });
 
 app.post("/audio/upload", async (c) => {
