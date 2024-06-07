714ce2fb4a6de99c092117d81d6c3ab448ff2281
shaammiru <msyamil.hamami@gmail.com>
2024-06-07 21:25:20 +0700

    feat: update hono and add timeout

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 import { serveStatic } from "hono/bun";
+import { timeout } from "hono/timeout";
 import imageHandler from "./handler/imageHandler";
 import audioHandler from "./handler/audioHandler";
 import videoHandler from "./handler/videoHandler";
@@ -15,6 +16,9 @@ app.use("/api/*", serveStatic({ root: "./" }));
 app.post("/api/image/resize", imageHandler.resize);
 app.post("/api/image/rotate", imageHandler.rotate);
 app.post("/api/image/flip", imageHandler.flip);
+
+app.use(timeout(10000));
+
 app.post("/api/audio/compress", audioHandler.compress);
 app.post("/api/video/compress", videoHandler.compress);
 
